fb1ccb5680135e7a383f7069bcc4a7316a368927
tjq <847678340@qq.com>
2022-06-04 16:27:17 +0800

    modify deploy

--- js/main.js
+++ js/main.js
@@ -1,136 +1,426 @@
-(function ($) {
-    // To top button
-    $("#back-to-top").on('click', function () {
-        $('body, html').animate({ scrollTop: 0 }, 600);
-    });
+var index = 4;//初始下标
+var indexArr= new Array();
+indexArr.push(1);
+indexArr.push(2);
+indexArr.push(3);
 
-    // Nav bar toggle
-    $('#main-nav-toggle').on('click', function () {
-        $('.nav-container-inner').slideToggle();
-    });
+function addRow() {
+    index++;
+    indexArr.push(index);
+    var showHtml = $("#travel_info_").html();
+    var html = '<div class="travel_info" id="travel_info'+index +'">'+showHtml+"</div>";
+    html = html.replace(/##/g,index);
+    $("#info").before($(html));
+    console.log(html);
+}
 
-    // Caption
-    $('.article-entry').each(function(i) {
-        $(this).find('img').each(function() {
-            if (this.alt && !(!!$.prototype.justifiedGallery && $(this).parent('.justified-gallery').length)) {
-                $(this).after('<span class="caption">' + this.alt + '</span>');
-            }
-
-            // 对于已经包含在链接内的图片不适用lightGallery
-            if ($(this).parent().prop("tagName") !== 'A') {
-                $(this).wrap('<a href="' + this.src + '" title="' + this.alt + '" class="gallery-item"></a>');
-            }
-        });
-
-    });
-    if (typeof lightGallery != 'undefined') {
-        var options = {
-            selector: '.gallery-item',
-        };
-        $('.article-entry').each(function(i, entry) {
-            lightGallery(entry, options);
-        });
-        lightGallery($('.article-gallery')[0], options);
+function deleteRow(inde){
+    $("#travel_info" + inde).remove();
+    var a = indexArr.indexOf(parseInt(inde));
+    if (a > -1) {
+        indexArr.splice(a, 1);
+        console.log("当前下标数组",indexArr);
     }
-    if (!!$.prototype.justifiedGallery) {  // if justifiedGallery method is defined
-        var options = {
-            rowHeight: 140,
-            margins: 4,
-            lastRow: 'justify'
-        };
-        $('.justified-gallery').justifiedGallery(options);
-    }
+}
 
-    // Sidebar expend
-    $('#sidebar .sidebar-toggle').on('click', function () {
-        if($('#sidebar').hasClass('expend')) {
-            $('#sidebar').removeClass('expend');
+
+ function json2Sheet () {
+    let json = [
+        {
+            "姓名": "张三",
+            "性别": "男",
+            "年龄": 18
+        },
+        {
+            "姓名": "李四",
+            "性别": "女",
+            "年龄": 19
+        },
+        {
+            "姓名": "王二麻",
+            "性别": "未知",
+            "年龄": 20
+        }
+    ]
+
+    // 实例化一个工作簿
+    let book = XLSX.utils.book_new()
+
+    // 实例化一个Sheet
+    let sheet = XLSX.utils.json_to_sheet(json, {
+        header: ['姓名', '性别', '年龄']
+    })
+
+    // 将Sheet写入工作簿
+    XLSX.utils.book_append_sheet(book, sheet, 'Sheet1')
+
+    // 写入文件，直接触发浏览器的下载
+    XLSX.writeFile(book, 'json2Sheet.xlsx')
+}
+
+
+function array2Sheet () {
+    let data = [
+        ['姓名', '性别', '年龄'],
+        ['张三', '男', '18'],
+        ['李四', '女', '19'],
+        ['王二麻', '未知', '20']
+    ]
+
+    // 实例化一个工作簿
+    let book = XLSX.utils.book_new()
+
+    // 实例化一个Sheet
+    let sheet = XLSX.utils.aoa_to_sheet(data)
+
+    // 将Sheet写入工作簿
+    XLSX.utils.book_append_sheet(book, sheet, 'Sheet1')
+
+    // 写入文件，直接触发浏览器的下载
+    XLSX.writeFile(book, 'array2Sheet.xlsx')
+}
+
+
+function parseExcel (fileDom) {
+    let file = fileDom.files[0]
+    let reader = new FileReader()
+    let rABS = typeof FileReader !== "undefined" && (FileReader.prototype||{}).readAsBinaryString
+    if (rABS) {
+        reader.readAsBinaryString(file)
+    } else {
+        reader.readAsArrayBuffer(file)
+    }
+    reader.onload = function(e) {
+        let data = e.target.result
+        if (!rABS) {
+            data = new Uint8Array(data)
+        }
+        let workBook = XLSX.read(data, {type: rABS ? 'binary' : 'array'})
+        workBook.SheetNames.forEach(name => {
+
+            let sheet = workBook.Sheets[name]
+            let json = XLSX.utils.sheet_to_json(sheet, {
+                raw: false,
+                header: 1
+            })
+
+            console.log(json);
+            let book = XLSX.utils.book_new();
+
+            // 实例化一个Sheet
+            let sheet1 = XLSX.utils.json_to_sheet(json);
+        
+            // 将Sheet写入工作簿
+            XLSX.utils.book_append_sheet(book, sheet, 'Sheet1');
+        
+            // 写入文件，直接触发浏览器的下载
+            XLSX.writeFile(book, 'json2Sheet.xlsx');       
+        })
+    }
+}
+
+let titleCellStyle = {
+    font: {name:"宋体",sz: 13,bold: true,color: { rgb: '000000' }},
+    alignment: {horizontal: 'center',vertical: 'center',wrap_text: 'false'},
+    border: {top: {style: "thin",},bottom: {style: "thin",},left: {style: "thin",},right: {style: "thin"}}
+};
+
+let defaultCellStyle = {
+    font: {name:"宋体",sz: 11,bold: false,color: { rgb: '000000' }},
+    alignment: {horizontal: 'center',vertical: 'center',wrap_text: 'false'},
+    border: {top: {style: "thin",},bottom: {style: "thin",},left: {style: "thin",},right: {style: "thin"}}
+};
+
+descCellStyle = {
+    font: {name:"宋体",sz: 11,bold: false,color: { rgb: '000000' }},
+    alignment: {horizontal: 'bottom',vertical: 'center',wrap_text: 'false'},
+    border: {top: {style: "thin",},bottom: {style: "thin",},left: {style: "thin",},right: {style: "thin"}}
+};
+
+headerCellStyle = {
+    font: {name:"宋体",sz: 11,bold: true,color: { rgb: '000000' }},
+    alignment: {horizontal: 'center',vertical: 'center',wrap_text: 'false'},
+    border: {top: {style: "thin",},bottom: {style: "thin",},left: {style: "thin",},right: {style: "thin"}}
+};
+
+secondCellStyle = {
+    font: {name:"宋体",sz: 11,bold: true,color: { rgb: '000000' }},
+    alignment: {horizontal: 'bottom',vertical: 'center',wrap_text: 'false'},
+    border: {top: {style: "thin",},bottom: {style: "thin",},left: {style: "thin",},right: {style: "thin"}}
+};
+
+lastCellStyle = {
+    font: {name:"宋体",sz: 11,bold: false,color: { rgb: '000000' }},
+    alignment: {horizontal:'top',vertical: 'center',wrap_text: 'true'},
+    border: {top: {style: "thin",},bottom: {style: "thin",},left: {style: "thin",},right: {style: "thin"}}
+};
+
+
+function openDownloadDialog(url, saveName) {
+    if (typeof url == 'object' && url instanceof Blob) {
+        url = URL.createObjectURL(url); // 创建blob地址
+    }
+    var aLink = document.createElement('a');
+    aLink.href = url;
+    aLink.download = saveName || ''; // HTML5新增的属性，指定保存文件名，可以不要后缀，注意，file:///模式下不会生效
+    var event;
+    if (window.MouseEvent) event = new MouseEvent('click');
+    else {
+        event = document.createEvent('MouseEvents');
+        event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0,
+            null);
+    }
+    aLink.dispatchEvent(event);
+}
+
+
+function sheet_from_array_of_arrays(data) {
+    const ws = {};
+    const range = {
+      s: {
+        c: 10000000,
+        r: 10000000
+      },
+      e: {
+        c: 0,
+        r: 0
+      }
+    };
+    for (let R = 0; R !== data.length; ++R) {
+      for (let C = 0; C !== data[R].length; ++C) {
+        if (range.s.r > R) range.s.r = R;
+        if (range.s.c > C) range.s.c = C;
+        if (range.e.r < R) range.e.r = R;
+        if (range.e.c < C) range.e.c = C;
+        let cell={};
+        if(R==0 && C==0){
+            cell = {
+                v: data[R][C],
+                s: titleCellStyle
+            };
+        }else if(R==1){
+            cell = {
+                v: data[R][C],
+                s: descCellStyle
+            };
+        }else if(R==2){
+            cell = {
+                v: data[R][C],
+                s: headerCellStyle
+            };
+        }else if(R==data.length-2){
+            cell = {
+                v: data[R][C],
+                s: secondCellStyle
+            };
+        }else if(R==data.length-1 && C==0){
+            cell = {
+                v: data[R][C],
+                s: lastCellStyle
+            };
+        }else{
+            cell = {
+                v: data[R][C],
+                s: defaultCellStyle
+            };
+        } 
+
+        if (cell.v == null) {
+            continue;
+        }
+        const cell_ref = XLSX.utils.encode_cell({
+          c: C,
+          r: R
+        });
+        /* TEST: proper cell types and value handling */
+        if (typeof cell.v === 'number') {
+          cell.t = 'n';
+        } else if (typeof cell.v === 'boolean') {
+          cell.t = 'b';
+        } else if (cell.v instanceof Date) {
+          cell.t = 'n';
+          cell.z = XLSX.SSF._table[14];
+          cell.v = this.dateNum(cell.v);
         } else {
-            $('#sidebar').addClass('expend');
+          cell.t = 's';
         }
-    });
+        ws[cell_ref] = cell;
+      }
+    }
+    /* TEST: proper range */
+    if (range.s.c < 10000000) {
+        ws['!ref'] = XLSX.utils.encode_range(range);
+    }
+    return ws;
+}
 
 
-    // Remove extra main nav wrap
-    $('.main-nav-list > li').unwrap();
+function sheet2blob(sheet){
+    sheetName = "sheet1";
+    const workbook = {SheetNames:[sheetName],Sheets:{}};
+    workbook.Sheets[sheetName]=sheet;
+    const wopts ={bookType:"xlsx",bookSST:false,type:'binary'};
+    const wbout= XLSX.write(workbook,wopts,{defaultCellStyle:defaultCellStyle});
+    const blob =new Blob([s2ab(wbout)],{type:"application/octet-stream"});
 
-    // Highlight current nav item
-    $('#main-nav > li > .main-nav-list-link').each(function () {
-        if($('.page-title-link').length > 0){
-            if ($(this).html().toUpperCase() == $('.page-title-link').html().toUpperCase()) {
-                $(this).addClass('current');
-            } else if ($(this).attr('href') == $('.page-title-link').attr('data-url')) {
-                $(this).addClass('current');
-            }
+    function s2ab(s){
+        const buf = new ArrayBuffer(s.length);
+        const view = new Uint8Array(buf);
+        for (let i=0;i!=s.length;i++){
+            view[i]=s.charCodeAt(i)&0xFF;
         }
-    });
+        return buf;
+    }
+    return blob;
+}
 
-    // Auto hide main nav menus
-    function autoHideMenus(){
-        var max_width = $('.nav-container-inner').width() - 10;
-        var main_nav_width = $('#main-nav').width();
-        var sub_nav_width = $('#sub-nav').width();
-        if (main_nav_width + sub_nav_width > max_width) {
-            // If more link not exists
-            if ($('.main-nav-more').length == 0) {
-                $(['<li class="main-nav-list-item top-level-menu main-nav-more">',
-                    '<a class="main-nav-list-link" href="javascript:;">More</a>',
-                    '<ul class="main-nav-list-child">',
-                    '</ul></li>'].join('')).appendTo($('#main-nav'));
-                // Bind hover event
-                $('.main-nav-more').hover(function () {
-                    if($(window).width() < 480) {
-                        return;
-                    }
-                    $(this).children('.main-nav-list-child').slideDown('fast');
-                }, function () {
-                    if($(window).width() < 480) {
-                        return;
-                    }
-                    $(this).children('.main-nav-list-child').slideUp('fast');
-                });
-            }
-            var child_count = $('#main-nav').children().length;
-            for (var i = child_count - 2; i >= 0; i--) {
-                var element = $('#main-nav').children().eq(i);
-                if (main_nav_width + sub_nav_width > max_width) {
-                    element.prependTo($('.main-nav-more > ul'));
-                    main_nav_width = $('#main-nav').width();
-                } else {
-                    return;
+function submit(){
+    let all_data = [];
+    
+    var selector1  = document.getElementById("company");
+    var company_index=selector1.selectedIndex;
+    var company = selector1.options[company_index].text;
+
+    var selector2  = document.getElementById("car_type");
+    var car_type_index=selector2.selectedIndex;
+    var car_type = selector2.options[car_type_index].text;
+
+    var selector3  = document.getElementById("car_number");
+    var car_number_index=selector3.selectedIndex;
+    var car_number = selector3.options[car_number_index].text;
+
+    var person  = document.getElementById("person").value;
+    console.log(person);
+
+    
+    var date = new Date();
+    var year = date.getFullYear();
+    title = company+"\n"+year+"年用车清单";
+    description = "用车单位："+company+"    "+person;
+    console.log(description);
+
+
+    console.log(title);
+    temp_data = [];
+    temp_data.push(title);
+    for(var t=0;t<14;t++){
+        temp_data.push("");
+    }
+    all_data.push(temp_data);
+
+    temp_data1 = [];
+    temp_data1.push(description);
+    for(var t=0;t<14;t++){
+        temp_data1.push("");
+    }
+    all_data.push(temp_data1);
+    
+    console.log(car_type);
+
+    table_header = ["序号","日期","车型","车牌","用车时间","基本行程","租金","油费","路费","停车费","超时","超公里","餐费","住宿费","合计"];
+    all_data.push(table_header);
+    console.log(car_number);
+    
+    var num_of_null =0
+
+    for (let i = 0; i < indexArr.length; i++) {
+        var info_temp = "#travel_info"+indexArr[i];
+        console.log(info_temp);
+        var data = [];
+        data.push(i+1);
+        var Input = $(info_temp).find("input");
+        
+        if(Input[0].value==""){
+            num_of_null = num_of_null+1;
+            continue;
+        }
+        var total = 0;
+
+        for (var j = 0;j<Input.length;j++){
+            if(i==0){
+                if(j==1){
+                    data.push(car_type);
+                    data.push(car_number);
+                    data.push(Input[j].value);
+                    
+                }else{
+                    data.push(Input[j].value);
                 }
+                if(j>=3){
+                    total = total + Number(Input[j].value);
+                }
+            }else{
+                    if(j==1){
+                        data.push("");
+                        data.push("");
+                        data.push(Input[j].value);
+                        
+                    }else{
+                            data.push(Input[j].value);
+                    }
+                    
+                    if(j>=3){
+                        total = total + Number(Input[j].value);
+                    }
             }
         }
-        // Nav bar is wide enough
-        if ($('.main-nav-more').length > 0) {
-            $('.main-nav-more > ul').children().appendTo($('#main-nav'));
-            $('.main-nav-more').remove();
+        data.push("");
+
+        all_data.push(data);
+    }
+    all_data.push(["","","","","","","","","","","","","","",""]);
+
+    all_data.push(["合计","","","","","","","","","","","","","",""]);
+    all_data.push(["备注：账户名称：广州广源汽车租赁有限公司    银行账号：1209-1816-4310-902    开户行：招商银行淘金支行，需转费用     元。\n●请贵单位转账时备注用车单据号（052224）","","","","","","","","","","","","","",""]);
+    all_data.push(["广州广源汽车租赁有限公司（财务部）","","","","","","","","","","","","","",""]);
+
+    console.log(all_data);
+
+    let book = XLSX.utils.book_new();
+
+    // 实例化一个Sheet
+    let sheet = sheet_from_array_of_arrays(all_data);
+
+    
+    const merge = [{s:{r:1,c:0},e:{r:1,c:14}},
+                   {s:{r:0,c:0},e:{r:0,c:14}},
+                   {s:{r:3,c:2},e:{r:2+indexArr.length-num_of_null,c:2}},
+                   {s:{r:3,c:3},e:{r:2+indexArr.length-num_of_null,c:3}},
+                   {s:{r:3+indexArr.length-num_of_null,c:0},e:{r:3+indexArr.length-num_of_null,c:14}},
+                   {s:{r:4+indexArr.length-num_of_null,c:0},e:{r:4+indexArr.length-num_of_null,c:5}},
+                   {s:{r:5+indexArr.length-num_of_null,c:0},e:{r:5+indexArr.length-num_of_null,c:14}},
+                   {s:{r:6+indexArr.length-num_of_null,c:0},e:{r:6+indexArr.length-num_of_null,c:14}},
+                ];
+    
+    sheet["!merges"] = merge;
+
+    sheet['!cols'] = [{ wpx: 44 }, { wpx: 68 }, { wpx: 68 }, { wpx: 83 }, { wpx: 75 },{ wpx: 102 }, { wpx: 50 }, { wpx: 50 }, { wpx: 50 }, { wpx: 50 },{ wpx: 50 }, { wpx: 50 }, { wpx: 50 }, { wpx: 50 }, { wpx: 50 }];
+    sheet['!rows']=[];
+    for(var m=0;m<4+indexArr.length;m++){
+        if(m==1){
+            sheet['!rows'].push({ hpx: 26});
+        }else{
+            sheet['!rows'].push({ hpx: 35});
         }
     }
-    autoHideMenus();
+    sheet['!rows'].push({ hpx: 35});
+    sheet['!rows'].push({ hpx: 45});
+    sheet['!rows'].push({ hpx: 35});
 
-    $(window).on('resize', function () {
-        autoHideMenus();
-    });
+    //最终调整格式
+    
 
-    // Fold second-level menu
-    $('.main-nav-list-item').hover(function () {
-        if ($(window).width() < 480) {
-            return;
-        }
-        $(this).children('.main-nav-list-child').slideDown('fast');
-    }, function () {
-        if ($(window).width() < 480) {
-            return;
-        }
-        $(this).children('.main-nav-list-child').slideUp('fast');
-    });
+    console.log(sheet);
 
-    // Add second-level menu mark
-    $('.main-nav-list-item').each(function () {
-        if ($(this).find('.main-nav-list-child').length > 0) {
-            $(this).addClass('top-level-menu');
-        }
-    });
+    // 将Sheet写入工作簿
+    XLSX.utils.book_append_sheet(book, sheet, 'Sheet1');
 
-})(jQuery);
+    // 写入文件，直接触发浏览器的下载
+    XLSX.writeFile(book, 'array2Sheet.xlsx');
+
+}
+
+
+
+
+
